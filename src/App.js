8e3a580eaeb9af621b4839930c773fb827f3bb73
CrystianBM <cbm2003x1@gmail.com>
2024-11-04 00:09:09 -0300

    testing vercel database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/database')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data from PostgreSQL:</h1>
+      {data ? <p>{JSON.stringify(data)}</p> : <p>Loading...</p>} 
     </div>
   );
 }
---
+++ src/api/database.js
@@ -0,0 +1,26 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+export default async function handler(req, res) {
+    try {
+      const result = await pool.query('SELECT * FROM Players'); // Example query
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Database error' });
+    }
+  }
+
+export async function insertDataPlayer(values) {
+    // Insert a new row into a table
+    await pool.query(
+        'INSERT INTO Players (charactername, characterage, charactergender, characterlook, charactertendency, '
+        + 'attstrength, attvigor, attagility, attintelligence, attperception, attknowledge, attcharisma) '
+        + 'VALUES (value[0], value[1], value[2], value[3], value[4], value[5], value[6], value[7], value[8], value[9], value[10], value11])'
+    );
+}
