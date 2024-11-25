1613978f8f9ea9239a5738a2754d49929c27b30e
CrystianBM <cbm2003x1@gmail.com>
2024-11-24 22:57:47 -0300

    testing db from vercel lib

--- src/api/database.js
+++ src/api/database.js
@@ -1,26 +1,13 @@
-import pg from 'pg';
+import { db } from "@vercel/postgres";
 
-const { Pool } = pg;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-})
 
 export default async function handler(req, res) {
-    try {
-      const result = await pool.query('SELECT * FROM players'); // Example query
-      res.status(200).json(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Database error' });
-    }
-  }
+  const client = await db.connect()
 
-export async function insertDataPlayer(values) {
-    // Insert a new row into a table
-    await pool.query(
-        'INSERT INTO Players (charactername, characterage, charactergender, characterlook, charactertendency, '
-        + 'attstrength, attvigor, attagility, attintelligence, attperception, attknowledge, attcharisma) '
-        + 'VALUES (value[0], value[1], value[2], value[3], value[4], value[5], value[6], value[7], value[8], value[9], value[10], value11])'
-    );
+  try {
+    const data = await client.sql`SELECT * FROM players`
+    return res.status(200).json({ data })
+  } catch(error) {
+    return res.status(500).json({ error })
+  }
 }
